2b09bfe758f8165ccf5fcf8e341c57e2b1195df6
Kent Erik Hole <kent.erik.hole@outlook.com>
2024-11-22 00:10:27 +0100

    new method for routering

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const stock = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/inventory.json`)
 );
 
-app.get('/api/v1/stock', (req, res) => {
+const getAllStock = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: stock.length,
@@ -17,11 +17,9 @@ app.get('/api/v1/stock', (req, res) => {
       stock
     }
   });
-});
+};
 
-// Get by ID
-app.get('/api/v1/stock/:id', (req, res) => {
-  // console.log(req.params);
+const getStock = (req, res) => {
   const id = req.params.id * 1;
   const stockid = stock.find(el => el.id === id);
 
@@ -39,43 +37,12 @@ app.get('/api/v1/stock/:id', (req, res) => {
       stockid
     }
   });
-});
+};
 
-app.patch('/api/v1/stock7:id', (req, res) => {
-  if (req.params.id * 1 > stock.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      stock
-    }
-  });
-});
-
-app.delete('/api/v1/stock7:id', (req, res) => {
-  if (req.params.id * 1 > stock.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-});
-
-app.post('/api/v1/stock', (req, res) => {
-  // console.log(req.body);
-
+const createStock = (req, res) => {
   const newId = stock[stock.length - 1].id + 1;
   const newStock = Object.assign({ id: newId }, req.body);
+
   stock.push(newStock);
 
   fs.writeFile(
@@ -90,6 +57,54 @@ app.post('/api/v1/stock', (req, res) => {
       });
     }
   );
-});
+};
+
+const updateStock = (req, res) => {
+  if (req.params.id * 1 > stock.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stock
+    }
+  });
+};
+
+const deleteStock = (req, res) => {
+  if (req.params.id * 1 > stock.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
+
+app
+  .route('/api/v1/stock')
+  .get(getAllStock)
+  .post(createStock);
+
+app
+  .route('/api/v1/stock/:id')
+  .get(getStock)
+  .patch(updateStock)
+  .delete(deleteStock);
 
 module.exports = app;
+
+// Old Metode
+//app.get('/api/v1/stock', getAllStock);
+//app.get('/api/v1/stock/:id', getStock);
+//app.post('/api/v1/stock', createStock);
+//app.patch('/api/v1/stock7:id', updateStock);
+//app.delete('/api/v1/stock7:id', deleteStock);
